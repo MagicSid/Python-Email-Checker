62a44b2887d7ee26cc64c16530648fd98254817e
Jake Cullum <jakesgamig@gmail.com>
2020-06-30 02:11:58 +0100

    Update Python-Email-Checker.py
    first checks to prefix added, still need appropriate validity checks for ., .. and quoted ..

--- Python-Email-Checker.py
+++ Python-Email-Checker.py
@@ -1,21 +1,31 @@
 import re
 
+#Legitimate Character Sets "[a-z][0-9][!#\$%&'\*\+/=?\^_`\{\|\}~-]"
 
 def check(inputText):
     """Check if parameter is a valid email and returns true or false"""
+    #First sanitise input by lowercasing the input
+    inputText = inputText.lower()
     isValidPrefix = False
     isValidSuffix = False
     split = re.split("@", inputText)
-    if(len(split) != 2 or len(suffix) > 253):
+    if len(split) != 2:
         return False
+    
     prefix = split[0]
     suffix = re.split(".",split[1])
+    if len(split[1]) > 253 or len(prefix) > 64:
+        return False
+    
     #Check prefix of address.
-    if():
+    legitChars = "(([a-z]*[0-9]*[!#$%&'*+/=?^_`{|}~-]*)*)"
+    testpre = re.sub(legitChars,"",prefix)
+    print(testpre)
+    if(testpre != ""):
         return False
     #Now check Suffix
-    for(domain in suffix):
-        if(domain > 63 or ):
+    for domain in suffix:
+        if(len(domain) > 63):
             return False
     #if All checks passed then simply return true
     return True
@@ -23,4 +33,7 @@ def check(inputText):
 
 
 print(check("bleh"))
-print(check("John@myspace.com"))
+print(check("Jo.hn@myspace.com"))
+print(check("\"Jo..hn\"@myspace.com"))
+print(check("Jo..hn@myspace.com"))
+print(check("!#$%&'*+-/=?^_`{|}~@bleh.com"))
