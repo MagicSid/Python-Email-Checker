c869b48f9a7a64ea5f9a7be60ca63cc1164477f2
Jake Cullum <jakesgamig@gmail.com>
2020-07-01 12:43:04 +0100

    Update Python-Email-Checker.py
    Tests added for suffixes and appropriate changes to the suffix part of the check so that it succeeds the tests

--- Python-Email-Checker.py
+++ Python-Email-Checker.py
@@ -13,7 +13,7 @@ def check(inputText):
         return False
     
     prefix = split[0]
-    suffix = re.split(".",split[1])
+    suffix = re.split("\.",split[1])
     if len(split[1]) > 253 or len(prefix) > 64 or prefix[0:1] ==".":
         return False
     
@@ -24,16 +24,23 @@ def check(inputText):
     if(testpre != ""):
         return False
     #Now check Suffix
+    legitChars = "(([a-z]*[0-9]*)*)"
+    legitCharsPlus = "(([a-z]+|[0-9]+)+)"
     for domain in suffix:
-        if(len(domain) > 63):
+        if(len(domain) > 63 or domain == "" or domain[0:1] == "-"):
+            return False
+        testsuf = re.sub("(\-" + legitCharsPlus + ")+|" + legitChars,"",domain)
+        if(testsuf != ""):
             return False
     #if All checks passed then simply return true
     return True
 
 #Test no @
+print("@ test")
 print("Should Fail:" + str(check("bleh")))
 
 #Test Prefix
+print("Prefix tests")
 print("Should succeed:" + str(check("Jo.hn@myspace.com")))
 print("Should succeed:" + str(check("J.o.hn@myspace.com")))
 print("Should Fail:" + str(check(".John@myspace.com")))
@@ -43,3 +50,11 @@ print("Should Fail:" + str(check("Jo..hn@myspace.com")))
 print("Should succeed:" + str(check("!#$%&'*+-/=?^_`{|}~@bleh.com")))
 
 #Test Suffix
+print("Suffix tests")
+print("Should succeed:" + str(check("John@myspace.com")))
+print("Should succeed:" + str(check("John@myspace.co-m")))
+print("Should fail:" + str(check("John@myspace-.com")))
+print("Should fail:" + str(check("John@-myspace.com")))
+print("Should fail:" + str(check("John@myspace.-com")))
+print("Should fail:" + str(check("John@myspace.com-")))
+
